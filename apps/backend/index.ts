f02b7228e58bf52c2c9115c8289aee6ecf26940c
HardikDudeja <hardik.dudeja39@gmail.com>
2025-02-27 00:37:44 +0530

    feat: setup backend express server with routes

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,12 +1,17 @@
+import { TrainModelSchema } from "common/types";
 import express from "express";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-})
+app.post("/ai/training", (req, res) => {});
+
+app.post("/ai/generate", (req, res) => {});
+
+app.post("/pack/generate", (req, res) => {});
+
+app.get("/pack/bulk", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
