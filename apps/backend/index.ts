594f15f37b800cd7909830d5a2f68c35280e0afb
HardikDudeja <hardik.dudeja39@gmail.com>
2025-03-02 02:00:08 +0530

    train model post request

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -5,8 +5,28 @@ import express from "express";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
 
-app.post("/ai/training", (req, res) => {});
+app.post("/ai/training", async (req, res) => {
+  const parsedBody = TrainModelSchema.safeParse(req.body);
+  if (!parsedBody.success) {
+    res.status(400).json({
+      error: "Invalid request body",
+      message: parsedBody,
+    });
+    return;
+  }
+  await prismaClient.model.create({
+    data: {
+      name: parsedBody.data.name,
+      type: parsedBody.data.type,
+      age: parsedBody.data.age,
+      ethnicity: parsedBody.data.ethnicity,
+      eyeColor: parsedBody.data.eyeColor,
+      bald: parsedBody.data.bald,
+    },
+  });
+});
 
 app.post("/ai/generate", (req, res) => {});
 
