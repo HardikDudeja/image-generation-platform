c5f7bc95c22d33c4ca23b8d5b5afb4c1ee69d55b
HardikDudeja <hardik.dudeja39@gmail.com>
2025-03-01 18:25:41 +0530

    add schema

--- packages/common/types.ts
+++ packages/common/types.ts
@@ -1,6 +1,32 @@
 import { z } from "zod";
 
 export const TrainModelSchema = z.object({
-    name: z.string(),
-    type: z.string(),
-})
+  name: z.string(),
+  type: z.enum(["Man", "Woman", "Other"]),
+  age: z.number(),
+  ethnicity: z.enum([
+    "Asian",
+    "Black",
+    "Hispanic",
+    "Indian",
+    "Middle Eastern",
+    "Native American",
+    "Pacific Islander",
+    "White",
+    "Other",
+  ]),
+  eyeColor: z.enum(["Brown", "Blue", "Green", "Hazel", "Gray", "Other"]),
+  bald: z.boolean(),
+  images: z.array(z.string()),
+});
+
+export const GenerateImageSchema = z.object({
+  prompt: z.string(),
+  modelId: z.string(),
+  n: z.number(),
+});
+
+export const GenerateImageFromPackSchema = z.object({
+  modelId: z.string(),
+  packId: z.string(),
+});
