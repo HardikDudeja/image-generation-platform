6e74815e167fa78d684f059b81baf78f89a57244
HardikDudeja <hardik.dudeja39@gmail.com>
2025-04-03 00:41:01 +0530

    some changes

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,4 +1,8 @@
-import { GenerateImageSchema, TrainModelSchema } from "common/types";
+import {
+  GenerateImageFromPackSchema,
+  GenerateImageSchema,
+  TrainModelSchema,
+} from "common/types";
 import { prismaClient } from "db";
 import express from "express";
 
@@ -47,32 +51,86 @@ app.delete("/ai/models", async (req, res) => {
   }
 });
 
-app.post("/ai/generate", async (req, res) => {
+app.post("/ai/generate", async (req, res): Promise<any> => {
   try {
     const parsedBody = GenerateImageSchema.safeParse(req.body);
     if (!parsedBody.success) {
       return res.status(400).json({ error: "Invalid request body" });
     }
-
     const { prompt, modelId } = parsedBody.data;
     const data = await prismaClient.outputImages.create({
       data: {
         prompt,
         modelId,
-        imageUrl: "",
       },
     });
     res
       .status(200)
       .json({ imageId: data.id, message: "Generation request created" });
   } catch (error) {
-    res.status(500).json({ error: "Server error" });
+    res.status(500).json({ error });
   }
 });
 
-app.post("/pack/generate", (req, res) => {});
+app.post("/pack/generate", async (req, res) => {
+  try {
+    const parsedBody = GenerateImageFromPackSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      res.status(400).json({ error: parsedBody.error });
+      return;
+    }
+    const prompts = await prismaClient.packPrompts.findMany({
+      where: {
+        packId: parsedBody.data.packId,
+      },
+    });
 
-app.get("/pack/bulk", (req, res) => {});
+    const images = await prismaClient.outputImages.createManyAndReturn({
+      data: prompts.map((prompt) => ({
+        prompt: prompt.prompt,
+        modelId: parsedBody.data.modelId,
+      })),
+    });
+    res.json({
+      images,
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+app.get("/pack/bulk", async (req, res) => {
+  try {
+    const packs = await prismaClient.packs.findMany({
+      include: {
+        prompts: true,
+      },
+    });
+    res.json({ packs });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+app.get("/image/bulk", async (req, res) => {
+  const images = req.query.images as string[];
+  if (!images || !Array.isArray(images)) {
+    res.status(400).json({ error: "Images array is required" });
+    return;
+  }
+  try {
+    const outputImages = await prismaClient.outputImages.findMany({
+      where: {
+        id: {
+          in: images,
+        },
+      },
+    });
+    res.json({ images: outputImages });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
